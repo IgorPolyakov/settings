9f2bff3f6837178e28576305387b4bde74adac58
Igor Polyakov <IgorPolyakov@users.noreply.github.com>
2017-04-07 12:20:32 +0700

    Update pre_install_software.sh

--- pre_install_software.sh
+++ pre_install_software.sh
@@ -1,8 +1,9 @@
 #/bin/bash
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install mc git zsh htop
+sudo apt update && sudo apt upgrade
+sudo apt install mc git zsh htop
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 echo "export ZSH=/home/$(whoami)/.oh-my-zsh
 ZSH_THEME="bira"
 plugins=(git rails bundle docker gitignore)
 source $ZSH/oh-my-zsh.sh" > ~/.zshrc
+chsh -s /bin/zsh
