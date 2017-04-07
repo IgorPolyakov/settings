9a84ea2773e828e8352d98f2f6550ff62c2ebcfb
Igor Polyakov <IgorPolyakov@users.noreply.github.com>
2017-04-07 12:18:04 +0700

    Update and rename software.sh to pre_install_software.sh

--- software.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install mc git zsh htop cppcheck cmake zenmap vlc meld dropbox astyle
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
