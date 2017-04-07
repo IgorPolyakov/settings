9543a8b0cd9e1934e0c3a83ca7a438c787e79e8f
Igor Polyakov <IgorPolyakov@users.noreply.github.com>
2017-04-07 12:03:47 +0700

    Create how_to_add_swap_space_on_ubuntu.sh

---
+++ how_to_add_swap_space_on_ubuntu.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+#How To Add Swap Space on Ubuntu 16.04
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo sysctl vm.swappiness=10
+sudo sysctl vm.vfs_cache_pressure=50
