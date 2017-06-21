1131143dc800bdf0bddb9f045765702c0382691b
Igor Polyakov <IgorPolyakov@users.noreply.github.com>
2017-06-21 18:41:22 +0700

    Update pre_install_desktop.sh

--- pre_install_desktop.sh
+++ pre_install_desktop.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 sudo apt update && sudo apt upgrade
-sudo apt install mc git zsh htop
+sudo apt install mc git zsh htop tmux
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 echo "export ZSH=/home/$(whoami)/.oh-my-zsh
 ZSH_THEME="bira"
